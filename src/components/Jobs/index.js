d3a3f32db1ce6ef9f44e4b3934d52e9c7f2c3b0e
amgothbhagyavathi <amgothbhagyavathi314@gmail.com>
2025-01-05 09:02:44 +0000

    Solution

--- src/components/Jobs/index.js
+++ src/components/Jobs/index.js
@@ -1,205 +1,265 @@
 import {Component} from 'react'
 import Cookies from 'js-cookie'
-import {AiFillStar} from 'react-icons/ai'
-import {MdLocationOn} from 'react-icons/md'
-import {BiLinkExternal} from 'react-icons/bi'
 import Loader from 'react-loader-spinner'
+import {BsSearch} from 'react-icons/bs'
 import Header from '../Header'
-import SimilarJobs from '../SimilarJobs'
+import FiltersGroup from '../FiltersGroup'
+import JobCard from '../JobCard'
+
+import './index.css'
+
+const employmentTypesList = [
+  {
+    label: 'Full Time',
+    employmentTypeId: 'FULLTIME',
+  },
+  {
+    label: 'Part Time',
+    employmentTypeId: 'PARTTIME',
+  },
+  {
+    label: 'Freelance',
+    employmentTypeId: 'FREELANCE',
+  },
+  {
+    label: 'Internship',
+    employmentTypeId: 'INTERNSHIP',
+  },
+]
+
+const salaryRangesList = [
+  {
+    salaryRangeId: '1000000',
+    label: '10 LPA and above',
+  },
+  {
+    salaryRangeId: '2000000',
+    label: '20 LPA and above',
+  },
+  {
+    salaryRangeId: '3000000',
+    label: '30 LPA and above',
+  },
+  {
+    salaryRangeId: '4000000',
+    label: '40 LPA and above',
+  },
+]
 
 const apiStatusConstants = {
   initial: 'INITIAL',
-  progress: 'PROGESS',
   success: 'SUCCESS',
   failure: 'FAILURE',
+  inProgress: 'IN_PROGRESS',
 }
 
 class Jobs extends Component {
   state = {
+    jobsList: [],
     apiStatus: apiStatusConstants.initial,
-    jobsData: [],
-    similarJobsData: [],
+    employeeTypeList: [],
+    minimumSalary: '',
+    searchInput: '',
   }
 
   componentDidMount() {
-    this.getJobData()
+    this.getJobs()
   }
 
-  getJobData = async () => {
-    const {match} = this.props
-    const {params} = match
-    const {id} = params
-
-    this.setState({apiStatus: apiStatusConstants.progress})
+  getJobs = async () => {
+    this.setState({
+      apiStatus: apiStatusConstants.inProgress,
+    })
+    const {employeeTypeList, minimumSalary, searchInput} = this.state
+    // console.log(employeeTypeList)
+    // employeeTypeList is empty array on initial page load when any input of type of employment is clicked
+    // we are setting state of this type in changeEmployeeList function
+    const apiUrl = `https://apis.ccbp.in/jobs?employment_type=${employeeTypeList.join()}&minimum_package=${minimumSalary}&search=${searchInput}`
+    // To convert a list of items as a comma-separated string we can use the array method join()
+    //  const fruits = ["Banana", "Orange", "Apple", "Mango"];
+    // console.log(fruits.join()) Banana,Orange,Apple,Mango
 
     const jwtToken = Cookies.get('jwt_token')
-    const url = `https://apis.ccbp.in/jobs/${id}`
-    const option = {
-      method: 'GET',
-      headers: {Authorization: `Bearer ${jwtToken}`},
-    }
-    const response = await fetch(url, option)
 
+    const options = {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      method: 'GET',
+    }
+    const response = await fetch(apiUrl, options)
     if (response.ok === true) {
       const data = await response.json()
-      const updatedData = [data.job_details].map(each => ({
-        companyLogoUrl: each.company_logo_url,
-        companyWebsiteUrl: each.company_website_url,
-        employmentType: each.employment_type,
-        id: each.id,
-        jobDescription: each.job_description,
-        lifeAtCompany: {
-          description: each.life_at_company.description,
-          imageUrl: each.life_at_company.image_url,
-        },
-        location: each.location,
-        packagePerAnnum: each.package_per_annum,
-        rating: each.rating,
-
-        skills: each.skills.map(eachSkill => ({
-          imageUrl: eachSkill.image_url,
-          name: eachSkill.name,
-        })),
-        title: each.title,
-      }))
-
-      const updatedSimilarData = data.similar_jobs.map(each => ({
-        companyLogoUrl: each.company_logo_url,
-        employmentType: each.employment_type,
-        id: each.id,
-        jobDescription: each.job_description,
-        location: each.location,
-        rating: each.rating,
-        title: each.title,
+      //  console.log(data.jobs) array of 60 objects
+      const updatedJobsData = data.jobs.map(eachJob => ({
+        companyLogoUrl: eachJob.company_logo_url,
+        employmentType: eachJob.employment_type,
+        id: eachJob.id,
+        jobDescription: eachJob.job_description,
+        location: eachJob.location,
+        packagePerAnnum: eachJob.package_per_annum,
+        rating: eachJob.rating,
+        title: eachJob.title,
       }))
       this.setState({
+        jobsList: updatedJobsData,
         apiStatus: apiStatusConstants.success,
-        jobsData: updatedData,
-        similarJobsData: updatedSimilarData,
       })
     } else {
-      this.setstate({apiStatus: apiStatusConstants.failure})
+      this.setState({
+        apiStatus: apiStatusConstants.failure,
+      })
     }
   }
 
-  successView = () => {
-    const {jobsData, similarJobsData} = this.state
-    if (jobsData.length >= 1) {
-      const {
-        companyLogoUrl,
-        companyWebsiteUrl,
-        employmentType,
-        jobDescription,
-        lifeAtCompany,
-        location,
-        packagePerAnnum,
-        rating,
-        skills,
-        title,
-      } = jobsData[0]
-      return (
-        <>
-          <div>
-            <div>
-              <img src={companyLogoUrl} alt="job details company logo" />
-              <div>
-                <h1>{title}</h1>
-                <div>
-                  <AiFillStar />
-                  <p>{rating}</p>
-                </div>
-              </div>
-            </div>
-            <div>
-              <MdLocationOn />
-              <p>{location}</p>
-            </div>
-            <div>
-              <p>{employmentType}</p>
-            </div>
-            <div>
-              <p>{packagePerAnnum}</p>
-            </div>
-            <hr />
-            <h1>Description</h1>
-            <a href={companyWebsiteUrl}>
-              Visit <BiLinkExternal />
-            </a>
-            <p>{jobDescription}</p>
-            <h1>Skills</h1>
-            <ul>
-              {skills.map(each => (
-                <li key={each.name}>
-                  <img src={each.imageUrl} alt={each.name} />
-                  <p>{each.name} </p>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div>
-            <h1>Life at Company</h1>
-            <p>{lifeAtCompany.description}</p>
-          </div>
-          <img src={lifeAtCompany.imageUrl} alt="Life at company" />
-          <h1>Similar Jobs</h1>
-          <ul>
-            {similarJobsData.map(each => (
-              <SimilarJobs
-                key={each.id}
-                similarDetails={each}
-                employmentType={employmentType}
-              />
-            ))}
-          </ul>
-        </>
-      )
-    }
-    return null
+  renderJobsList = () => {
+    const {jobsList} = this.state
+    const renderJobsList = jobsList.length > 0
+
+    return renderJobsList ? (
+      <div className="all-jobs-container">
+        <ul className="jobs-list">
+          {jobsList.map(job => (
+            <JobCard jobData={job} key={job.id} />
+          ))}
+        </ul>
+      </div>
+    ) : (
+      <div className="no-jobs-view">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/no-jobs-img.png"
+          className="no-jobs-img"
+          alt="no jobs"
+        />
+        <h1 className="no-jobs-heading">No Jobs Found</h1>
+        <p className="no-jobs-description">
+          We could not find any jobs. Try other filters.
+        </p>
+      </div>
+    )
   }
 
-  retryAgain = () => {
-    this.getJobData()
-  }
-
-  failureView = () => (
-    <div className="failure-container">
+  renderFailureView = () => (
+    <div className="jobs-error-view-container">
       <img
         src="https://assets.ccbp.in/frontend/react-js/failure-img.png"
         alt="failure view"
+        className="jobs-failure-img"
       />
-      <h1 className="heading">Oops! Something Went Wrong</h1>
-      <p>we cannot seem to find the page you are looking for</p>
-      <button type="button" className="button" onClick={this.retryAgain}>
-        retry
+      <h1 className="jobs-failure-heading-text">Oops! Something Went Wrong</h1>
+      <p className="jobs-failure-description">
+        We cannot seem to find the page you are looking for
+      </p>
+      <button
+        type="button"
+        data-testid="button"
+        className="jobs-failure-button"
+        onClick={this.getJobs}
+      >
+        Retry
       </button>
     </div>
   )
 
-  progressView = () => (
+  renderLoadingView = () => (
     <div className="loader-container" data-testid="loader">
       <Loader type="ThreeDots" color="#ffffff" height="50" width="50" />
     </div>
   )
 
-  renderProfile = () => {
+  renderAllJobs = () => {
     const {apiStatus} = this.state
+
     switch (apiStatus) {
       case apiStatusConstants.success:
-        return this.successView()
+        return this.renderJobsList()
       case apiStatusConstants.failure:
-        return this.failureView()
-      case apiStatusConstants.progress:
-        return this.progressView()
+        return this.renderFailureView()
+      case apiStatusConstants.inProgress:
+        return this.renderLoadingView()
       default:
         return null
     }
   }
 
+  changeSalary = salaryRangeId => {
+    // console.log(salary)
+    this.setState({minimumSalary: salaryRangeId}, this.getJobs)
+  }
+
+  changeEmployeeList = type => {
+    const {employeeTypeList} = this.state
+
+    const inputNotInList = employeeTypeList.filter(
+      eachItem => eachItem === type,
+    )
+    // console.log(inputNotInList)
+    if (inputNotInList.length === 0) {
+      this.setState(
+        prevState => ({
+          employeeTypeList: [...prevState.employeeTypeList, type],
+        }),
+        this.getJobs,
+      )
+    } else {
+      const filteredData = employeeTypeList.filter(
+        eachItem => eachItem !== type,
+      )
+      // console.log(filteredData)
+
+      this.setState({employeeTypeList: filteredData}, this.getJobs)
+    }
+  }
+
+  changeSearchInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  onEnterSearchInput = event => {
+    if (event.key === 'Enter') {
+      this.getJobs()
+    }
+  }
+
   render() {
+    const {searchInput} = this.state
     return (
       <>
         <Header />
-        <div>{this.renderProfile()}</div>
+        <div className="jobs-container">
+          <div className="jobs-content">
+            <FiltersGroup
+              employmentTypesList={employmentTypesList}
+              salaryRangesList={salaryRangesList}
+              changeSearchInput={this.changeSearchInput}
+              searchInput={searchInput}
+              getJobs={this.getJobs}
+              changeSalary={this.changeSalary}
+              changeEmployeeList={this.changeEmployeeList}
+            />
+            <div className="search-input-jobs-list-container">
+              <div className="search-input-container-desktop">
+                <input
+                  type="search"
+                  className="search-input-desktop"
+                  placeholder="Search"
+                  onChange={this.changeSearchInput}
+                  onKeyDown={this.onEnterSearchInput}
+                />
+                <button
+                  type="button"
+                  data-testid="searchButton"
+                  className="search-button-container-desktop"
+                  onClick={this.getJobs}
+                >
+                  <span className="visually-hidden">Search</span>
+                  <BsSearch className="search-icon-desktop" />
+                </button>
+              </div>
+              {this.renderAllJobs()}
+            </div>
+          </div>
+        </div>
       </>
     )
   }
